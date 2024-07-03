217124f9d4d1569cf66642a94e69d84b6e65a998
YuviPanda <yuvipanda@gmail.com>
2024-07-02 19:40:26 -0700

    Change ownership of Fiji app

--- setup-scripts/setup-fiji.bash
+++ setup-scripts/setup-fiji.bash
@@ -5,4 +5,5 @@ cd /tmp
 curl -sSL https://downloads.micron.ox.ac.uk/fiji_update/mirrors/fiji-latest/fiji-linux64.zip -o fiji.zip
 unzip fiji.zip
 mv Fiji.app /opt/Fiji.app
+chown -R ${NB_USER}:${NB_USEr} /opt/Fiji.app
 rm fiji.zip
